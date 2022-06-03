29924eac88173e59968427b5afc431bae72700a5
Pelumi <odunoyepelumi23@gmail.com>
2022-06-03 14:47:05 -0700

    4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - this contains all my code
+ * Return: void
+ */
+
+int main(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
