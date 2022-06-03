c074a96d1ee08ec57ac6994cd6334898e8791d1b
Pelumi <odunoyepelumi23@gmail.com>
2022-06-03 14:33:39 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is where the code is
+ * Returnv void
+ */
+
+int main(void)
+{
+	char c,
+	     
+	     for (c = 'a'; c <= 'z'; c++)
+		     if (c != 'e')
+			     if (c != 'q')
+				     putchar(c);
+	putchar('\n');
+
+	return (0);
+}
