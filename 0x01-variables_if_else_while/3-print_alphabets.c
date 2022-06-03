94b06af2a416cc5c7b350e93f5562920bf7ebb70
Pelumi <odunoyepelumi23@gmail.com>
2022-06-03 13:51:08 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/**
+ * main - this contains the code
+ * Return: void
+ */
+
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; C = 'A'; c <= 'z'; C <= 'Z'; c++; C++)
+	putchar(c);
+
+	putchar (C)
+
+	putchar('\n');
+
+	return (0);
+
+}
+
