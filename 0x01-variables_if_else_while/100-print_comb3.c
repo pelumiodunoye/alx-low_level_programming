74a97182bef9bb0c45ad1ea5a6e755a5dd8b24b0
Pelumi <odunoyepelumi23@gmail.com>
2022-06-03 16:06:14 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is my code here
+ * Return: void
+ */
+
+int main(void)
+{
+
+	int d;
+	int g;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+
+		for (g = '0'; g <= '9'; g++)
+		{
+			if (d != g && d < g)
+			{
+				putchar(d);
+				putchar(g);
+
+				if (d != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
