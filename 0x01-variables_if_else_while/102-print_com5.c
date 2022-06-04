00be7f7067c26b1abb917db6854ca82a5350999c
Pelumi <odunoyepelumi23@gmail.com>
2022-06-04 16:44:15 -0700

    c

---
+++ 0x01-variables_if_else_while/102-print_com5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is where all my code goes
+ * Return: void
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if (a + b < c + d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					}
+				}
+			}
+		}
+	}
+	
+	putchar(',');
+	putchar(' ');
+
+	return (0)
+}
