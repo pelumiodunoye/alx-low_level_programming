e92f27409220aadb54a1571dedd6f53ff93b8a51
Pelumi <odunoyepelumi23@gmail.com>
2022-06-04 15:07:28 -0700

    102-print_com5.c

--- 0x01-variables_if_else_while/102-print_com5.c
+++ 0x01-variables_if_else_while/102-print_com5.c
@@ -1,12 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
- * This is going to print the comnination of
- * two numbers so that none of them is repeated.
- * okay lets go
- */
-
 /**
  * main - this is thd main
  * Return: void
@@ -22,7 +16,6 @@ int main(void)
 
 	for (d = '0'; d <= '9'; d++)
 	{
-
 		for (g = '0'; g <= '9'; g++)
 		{
 			for (i = '0'; i <= '9'; i++)
@@ -51,4 +44,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
+
