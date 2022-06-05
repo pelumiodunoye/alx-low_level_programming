3dbb16b03b36bcf04bceb79ec4b96d9c8a4476aa
Pelumi <odunoyepelumi23@gmail.com>
2022-06-05 09:17:01 -0700

    x

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,6 @@ int main(void)
 	int b;
 	int c;
 	int d;
-	
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
@@ -23,14 +22,13 @@ int main(void)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-					if (a +b < c + d)
+					if (a + b < c + d)
 					{
 					putchar(a);
 					putchar(b);
 					putchar(' ');
 					putchar(c);
 					putchar(d);
-	
 			putchar(',');
 			putchar(' ');
 					}
