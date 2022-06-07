7f4cd2f28028bbefce3374f5cd42a73d741ad2e3
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 06:57:18 -0700

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this contains all the code
+ * Return: void
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar (c);
+			putchar ('\n');
+
+	return (0);
+}
