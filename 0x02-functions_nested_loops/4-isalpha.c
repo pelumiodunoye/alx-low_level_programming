1c3486a81c891e3af507f05e4e28a9097dba32ed
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 15:01:29 -0700

    f

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _islower - this is the main
+ * @i: this is the input
+ * Return: void
+ */
+
+int _isalpha(int i)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else if (i >= 'A' && i <= 'Z')
+		return (1);
+	else
+		return(0);
+}
