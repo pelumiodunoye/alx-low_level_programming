0383cfe1942c676aa285625aa626da711ed91679
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 10:32:18 -0700

    f

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _is_lower - this is the main
+ * Return: void
+ */
+
+int _is_lower(int i)
+{
+	if(i = 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
