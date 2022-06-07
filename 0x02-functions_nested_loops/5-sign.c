a6e3617f9a42db5f789d454caceac84516b54f1f
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 15:18:43 -0700

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _print_sign(int n) - this is the main
+ * @n: this is the input
+ * Return: void
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}
