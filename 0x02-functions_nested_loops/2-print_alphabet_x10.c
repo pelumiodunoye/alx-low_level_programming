aaeff024018e644977c662d24991c8b22cd293ad
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 09:03:46 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@
 void print_alphabet_x10(void)
 {
 	int a;
+	int i;
 
 	for (a = 0; a < 10; a++)
 	{
-		int i;
 
 		for (i = 'a'; i <= 'z'; i++)
 		{
-			putchar("%i", i);
+			putchar(i);
 			putchar('\n');
 		}
 	}
