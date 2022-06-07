805a210bc7bbdb81cfec8f0f305d108c99282130
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 15:45:23 -0700

    f

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n > '48')
 	{
-		putchar('+');
+		putchar('43');
 		return (1);
 	}
 
-	else if (n == 0)
+	else if (n == '48')
 	{
-		putchar('0');
+		putchar('48');
 		return (0);
 	}
 
 	else
 	{
-		putchar('-');
+		putchar('45');
 		return (-1);
 	}
 }
