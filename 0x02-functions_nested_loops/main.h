1c733bcf1eaf98c845252d3f4cfdb33537c1e872
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 07:01:23 -0700

    f

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int _putchar (char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _abs(int c);
+
+int print_last_digit(int);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
 
