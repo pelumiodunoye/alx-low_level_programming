7023ff1947c97809103ca8966bd877a90f8d5656
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 06:38:18 -0700

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+/**
+ * main - this is my code
+ * Return: void
+ */
+
+int main(void)
+{
+	char c = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		putchar(c);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+
+
+
+}
