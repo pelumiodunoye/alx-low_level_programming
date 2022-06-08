5963d0648b30c9a44491ff04d224844d257bc963
Pelumi <odunoyepelumi23@gmail.com>
2022-06-07 18:06:32 -0700

    f

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * add - add code
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of the two
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a + b);
+}
