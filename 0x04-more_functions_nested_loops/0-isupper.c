04511656ff5902e4eb264ce273e3c5c7a893d8b9
Pelumi <odunoyepelumi23@gmail.com>
2022-06-09 05:30:18 -0700

    f

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _isupper - checks the code
+ * @c: alphabets
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
+{
+	int c;
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
