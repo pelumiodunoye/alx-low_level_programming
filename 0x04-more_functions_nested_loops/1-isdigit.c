1345fd72f867b62d23c9655822c5c472c29a4be4
Pelumi <odunoyepelumi23@gmail.com>
2022-06-09 05:45:22 -0700

    f

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isdigit - checks the code
+ * @c: digit
+ * Return: 1 if c is a digit and 0 if c is not a digit
+ */
+
+int_isdigit(int c)
+{
+	if (c = 0 && c<= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
