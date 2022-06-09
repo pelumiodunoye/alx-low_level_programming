53fea8358c8885045501154628fd0fd042b4fa9b
Pelumi <odunoyepelumi23@gmail.com>
2022-06-09 16:17:39 -0700

    f

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * This code draws a straight line
+ * kowe
+ */
+
+/**
+ * print_line - code
+ * @n: length
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			putchar('_');
+	}
+
+	putchar('\n');
+}
