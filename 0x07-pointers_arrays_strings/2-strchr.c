335e6bca84ede22c141ec9dd8d9818426dd20056
Pelumi <odunoyepelumi23@gmail.com>
2022-06-20 05:51:15 -0700

    😔 🔫

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,17 +6,18 @@
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * if c is not found - NULL.
+ * If c is not found - NULL.
  */
 char *_strchr(char *s, char c)
 {
-	int index;
+	int i;
 
-	for (index = 0, s[index] >= '\0'; index++)
+	for (index = 0, *(s + i); i++)
 	{
-		if (s[index] == c)
-			return (s + index);
+		if (*(s + i) == c)
+			return (s + i);
 	}
+	
 
 	return ('\0');
 }
