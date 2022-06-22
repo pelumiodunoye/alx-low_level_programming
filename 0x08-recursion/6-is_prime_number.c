b7096581d1ea83e728b1995cbd71bd6a67638cd5
Pelumi <odunoyepelumi23@gmail.com>
2022-06-22 04:45:04 -0700

    Your eyes change colour when you lie

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * is_prime_number - checks if its a prime number
+ * @n: number to be checked
+ * Return: 1 if it's a prime number and 0 if its's not
+ */
+
+int is_prime_number(int n)
+{
+	int i = 4;
+
+	if (n % i == 0)
+		return (0);
+	else if (i == n)
+		return (1);
+}
+
