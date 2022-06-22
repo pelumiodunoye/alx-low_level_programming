72356b98ef037938bf544ba4f01cd10159fb6d86
Pelumi <odunoyepelumi23@gmail.com>
2022-06-22 04:28:11 -0700

    Yes, we're blessed but it's harder to see sometimes

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - raised to powerel of
+ * @x: base number
+ * @y: power
+ *
+ * Return: x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
