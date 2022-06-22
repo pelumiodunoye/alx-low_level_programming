d088a12880604cb59e4608c8d7a6eeab793fb297
Pelumi <odunoyepelumi23@gmail.com>
2022-06-22 04:56:47 -0700

    Your eyes change colour when you lie

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,43 @@
-/**
- * is_prime_number - checks if its a prime number
- * @n: number to be checked
- * Return: 1 if true and 0 if false
- */
+#include "main.h"
 
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ * If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number is to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ * If the number is prime - 1.
+ */
 int is_prime_number(int n)
 {
-	int i = 4;
+	int div = 2;
 
-	if (n % i == 0)
+	if (n <= 1)
 		return (0);
-	else if (i == n)
-		return (
 
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
+}
