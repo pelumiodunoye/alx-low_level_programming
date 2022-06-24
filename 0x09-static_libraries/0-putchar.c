17e2fbe807b072aeed8cffb4d70f83db10601748
Pelumi <odunoyepelumi23@gmail.com>
2022-06-24 15:49:29 -0700

    f

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+/**
+ * main - this is my code
+ * Return: void
+ */
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		putchar(c[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+
+
+
+}
